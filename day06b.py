eb512c69ba356c44499726cafcdbd0fb6b43cf6b
Matthew Huynh <matthewhuynh@fb.com>
2022-12-25 11:29:23 -0800

    day 6

---
+++ day06b.py
@@ -0,0 +1,38 @@
+# sliding window of X
+# output end if counter dictionary has X unique chars
+
+from collections import Counter
+
+
+start = 0; end = 0; windowSize = 14
+chars = Counter()
+line = ''
+with open('input.txt') as reader:
+    line = reader.readline().strip()
+
+while True:
+    c = line[end]
+    print(c)
+    if not c:
+        raise Exception("this should not be possible with valid input")
+    
+    # add 1 to this char's count
+    chars.update(c)
+
+    # shrink window
+    while end-start >= windowSize:
+        d = line[start]
+        chars.subtract(d)
+        if chars[d] == 0:
+            del chars[d]
+        start += 1
+
+    print(chars)
+
+    # check if 4 unique chars in window
+    if end-start+1 == windowSize and len(chars) == windowSize:
+        print(end+1, "characters must be processed")
+        exit()
+    
+    # expand window
+    end += 1
